e63422153bb2879814687a8bbdb74d9749a87c5e
ksdyb.park <ksdyb@amazon.com>
2023-04-30 23:28:08 +0900

    update

--- utils/lambda-generate-dataset/index.js
+++ utils/lambda-generate-dataset/index.js
@@ -101,7 +101,7 @@ exports.handler = async (event, context) => {
     }  
 
     let date = new Date();
-    const current = Math.floor(date.getTime()/1000);
+    const current = Math.floor(date.getTime()/1000.0);
     console.log('current: ', current);
 
     for(let i in dynamoQuery['Items']) {    
