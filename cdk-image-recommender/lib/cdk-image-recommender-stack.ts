17c4b01bb99d8caa34b9222a9e4a4eb2df99974a
ksdyb.park <ksdyb@amazon.com>
2023-05-06 08:17:30 +0900

    update

--- cdk-image-recommender/lib/cdk-image-recommender-stack.ts
+++ cdk-image-recommender/lib/cdk-image-recommender-stack.ts
@@ -21,7 +21,7 @@ const endpoints = [
   "jumpstart-example-model-txt2img-stabili-2023-04-29-02-00-20-092",
 ]
 const nproc = 1;
-const trackingId = "7ccb2f39-42ff-4c65-b4e9-5ea58ab56e50";
+const trackingId = "478f24dc-a983-4737-ba8d-70c8f56d6db1";
 
 export class CdkImageRecommenderStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
